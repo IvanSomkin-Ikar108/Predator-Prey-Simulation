4c7e6bfede939a23a4dc1bb27ffffc24aa8795d3
Ivan Somkin <somkin.van@yandex.ru>
2022-11-19 15:41:26 +0300

    Add migration calculation through linalg.solve

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ class Area:
         self.b = (b if b is not None else random.uniform(0.5, 1.))
         self.c = (c if c is not None else random.uniform(1., 5.))
         self.w_coef = w_coef
-        self.t = 0
 
     @staticmethod
     def hexagon_generator(img_x: int, img_y: int, hexagon_width: int):
@@ -44,6 +43,7 @@ class Area:
 
     def draw(self, draw: ImageDraw, hexagon_width: int):
         img_x, img_y = self.img_pos
+        text_size = hexagon_width // 7
         hexagon = self.hexagon_generator(img_x, img_y, hexagon_width)
         draw.polygon(list(hexagon), outline='red', fill='white')
 
@@ -51,26 +51,28 @@ class Area:
         text_offset_x = hexagon_width // 4
         text_offset_y = hexagon_width // 4
         draw.text((img_x - text_offset_x, img_y - text_offset_y), text=text, fill="black",
-                  font=ImageFont.truetype("arial.ttf", size=16), align="left")
+                  font=ImageFont.truetype("arial.ttf", size=text_size), align="left")
 
     def a(self, t: float):
         return self.a0 * (1 + math.sin(self.w_coef * math.pi * t))
 
-    def reproduce_interact_next(self, time_step: float, data_precision: int):
-        self.predator = self.predator_next
-        self.prey = self.prey_next
-        self.t += time_step
+    def reproduce_interact(self, time: float, time_step: float, data_precision: int):
+        t = time
         x = self.prey
         y = self.predator
-        a = self.a(self.t)
+        a = self.a(t)
         b = self.b
         c = self.c
         dt = time_step
         x_next = x * (1 + a * dt - y * dt)
         y_next = y * (1 + b * x * dt - c * dt)
-        self.prey_next = round(x_next, data_precision)
-        self.predator_next = round(y_next, data_precision)
-
+        if x_next < 0:
+            x_next = 0
+        if y_next < 0:
+            y_next = 0
+        self.prey = round(x_next, data_precision)
+        self.predator = round(y_next, data_precision)
+    '''
     def migrate_next(self, time_step: float, data_precision: int,
                      predator_migration_rate: float = 0, prey_migration_rate: float = 0):
         predator_diff = sum([n.predator - self.predator for n in self.neighbors])
@@ -78,11 +80,7 @@ class Area:
         self.predator_next = round(self.predator + predator_migration_rate * predator_diff, data_precision)
         self.prey_next = round(self.prey + prey_migration_rate * prey_diff, data_precision)
         self.t += time_step
-
-    def move_to_next(self):
-        self.predator = self.predator_next
-        self.prey = self.prey_next
-
+    '''
     def initialize_neighbors(self, areas: []):
         self.neighbors.clear()
         neighbor_positions: [] = self.calculate_neighbor_positions()
@@ -139,7 +137,7 @@ class PredatorPreyRunResult:
         p.plot(t, prey, label='Жертвы')
         p.plot(t, predator, label='Хищники')
         if max(t) > 5:
-            p.xticks(np.arange(0, max(t) + 1, 1.0), rotation=90, fontsize=10)
+            p.xticks(np.arange(0, max(t) + 1, max(t) // 20), rotation=90, fontsize=10)
         p.grid(True)
         p.legend()
         p.title(
@@ -170,6 +168,8 @@ class PredatorPreySimulation:
         self.run_result = dict()
         self.predator_migration_rate = predator_migration_rate
         self.prey_migration_rate = prey_migration_rate
+        self.predator_migration_matrix = None
+        self.prey_migration_matrix = None
 
     def initialize_areas_with_image(self, img_path):
         with Image.open(img_path) as im:
@@ -185,7 +185,8 @@ class PredatorPreySimulation:
                 for net_y in range(0, net_height, 1):
                     img_x = net_x * net_measure_x
                     img_y = net_y * net_measure_y + self.hexagon_column_offset_y * (net_x % 2)
-                    if img_x > 0 and img_y > 0 and self.is_pixel_black(img_bw, (img_x, img_y)):
+                    if (img_x < img_bw.width and img_y < img_bw.height
+                            and self.is_pixel_black(img_bw, (img_x, img_y))):
                         self.areas.append(Area((net_x, net_y), (img_x, img_y)))
 
             for a in self.areas:
@@ -198,10 +199,10 @@ class PredatorPreySimulation:
     def set_output_directory(self, output_directory):
         self.output_directory = output_directory
 
-    def add_run_result(self, a: Area):
+    def add_run_result(self, a: Area, t: int):
         self.run_result[a.id].predator.append(a.predator)
         self.run_result[a.id].prey.append(a.prey)
-        self.run_result[a.id].t.append(a.t)
+        self.run_result[a.id].t.append(t)
 
     def initialize_run_result(self, time_precision: int, data_precision: int):
         self.run_result = dict()
@@ -209,6 +210,27 @@ class PredatorPreySimulation:
             a = self.areas[i]
             a.id = i
             self.run_result[i] = PredatorPreyRunResult(a, time_precision, data_precision)
+
+    def get_migration_matrix(self, migration_rate: float, time_step: float):
+        matrix = np.zeros((len(self.areas), len(self.areas)), int).astype('float')
+        for area in self.areas:
+            matrix[area.id][area.id] = 1. + 2. * migration_rate * time_step
+            for neighbour in area.neighbors:
+                matrix[area.id][neighbour.id] = -1. * migration_rate * time_step
+        return matrix
+
+    def initialize_migration_matrices(self, time_step: float):
+        self.predator_migration_matrix = self.get_migration_matrix(self.predator_migration_rate, time_step)
+        self.prey_migration_matrix = self.get_migration_matrix(self.prey_migration_rate, time_step)
+
+    def migrate(self):
+        prey_old = [a.prey for a in self.areas]
+        predator_old = [a.predator for a in self.areas]
+        prey_new = np.linalg.solve(self.prey_migration_matrix, prey_old)
+        predator_new = np.linalg.solve(self.predator_migration_matrix, predator_old)
+        for i in range(0, len(self.areas)):
+            self.areas[i].prey = prey_new[i]
+            self.areas[i].predator = predator_new[i]
 
     def render_step(self, img_path: str):
         area_img = Image.new(
@@ -224,11 +246,24 @@ class PredatorPreySimulation:
             a.draw(draw, self.hexagon_width)
         area_img.save(img_path)
 
+    @staticmethod
+    def get_condition_number(matrix):
+        # np.linalg.norm(matrix) * np.linalg.norm(np.linalg.inv(matrix))
+        return np.linalg.cond(matrix)
+
     def run(self, steps: int, time_precision: int, data_precision: int,
             render: bool, render_step_period: int):
 
+        time = 0
         time_step = math.pow(10, -time_precision)
         self.initialize_run_result(time_precision, data_precision)
+        self.initialize_migration_matrices(time_step)
+
+        prey_condition_number = self.get_condition_number(self.prey_migration_matrix)
+        predator_condition_number = self.get_condition_number(self.predator_migration_matrix)
+
+        print("Число обусловленности для миграции жертв : ", prey_condition_number)
+        print("Число обусловленности для миграции хищников : ", predator_condition_number)
 
         run_directory = "result"
 
@@ -238,30 +273,21 @@ class PredatorPreySimulation:
             os.mkdir(run_directory)
 
         for step in range(0, steps):
+            if step % 2 == 0:
+                for a in self.areas:
+                    a.reproduce_interact(time, time_step, data_precision)
+            else:
+                self.migrate()
 
+            time += time_step
             for a in self.areas:
-                if step % 2 == 0:
-                    a.reproduce_interact_next(time_step, data_precision)
-                else:
-                    a.migrate_next(time_step, data_precision, self.predator_migration_rate, self.prey_migration_rate)
-
-            for a in self.areas:
-                a.move_to_next()
-                self.add_run_result(a)
+                self.add_run_result(a, time)
 
             if render and step % render_step_period == 0:
                 self.render_step(f'{run_directory}/step_{step}.jpg')
 
     def get_run_result(self, area_id: int):
         return self.run_result[area_id]
-
-    def get_migration_matrix(self, migration_rate: float, time_step: float):
-        matrix = np.zeros((len(self.areas), len(self.areas)), int).astype('float')
-        for area in self.areas:
-            matrix[area.id][area.id] = 1. + 2. * migration_rate * time_step
-            for neighbour in area.neighbors:
-                matrix[area.id][neighbour.id] = -1. * migration_rate * time_step
-        return matrix
 
     def print_migration_matrix_abstraction(self, v: float, time_step: float):
         for area in self.areas:
@@ -273,12 +299,11 @@ class PredatorPreySimulation:
 
 
 if __name__ == '__main__':
-    simulation = PredatorPreySimulation(120, predator_migration_rate=0.1, prey_migration_rate=0.1)
+    simulation = PredatorPreySimulation(120, predator_migration_rate=1, prey_migration_rate=1)
     simulation.initialize_areas_with_image('./assets/brazil.png')
     simulation.set_output_directory('result')
-    simulation.run(steps=1, time_precision=2, data_precision=3, render=True, render_step_period=1)
-    simulation.get_area_matrix()
-    # simulation.get_run_result(48).add_figure(plt, 0)
-    # simulation.get_run_result(51).add_figure(plt, 1)
-    # simulation.get_run_result(49).add_figure(plt, 2)
+    simulation.run(steps=10, time_precision=2, data_precision=3, render=True, render_step_period=1)
+    simulation.get_run_result(48).add_figure(plt, 0)
+    simulation.get_run_result(51).add_figure(plt, 1)
+    simulation.get_run_result(49).add_figure(plt, 2)
     plt.show()
